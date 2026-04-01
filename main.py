88aa392e2fd9bf507007399a12d0d2a24785b8d0
canberkkurtul <canberkkurtul@gmail.com>
2026-04-02 01:34:41 +0300

    01.04

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import pandas as pd
 import numpy as np
 import random
 
-i=4
-torch.manual_seed(i)
-random.seed(i)
-np.random.seed(i)
+#train_model(X, y, X_val, y_val, d_hidden=32, epochs=50, batch_size=500, lr=1e-3) i=16
+seed = 16
+torch.manual_seed(seed)
+random.seed(seed)
+np.random.seed(seed)
 
 class FNN(nn.Module):
     def __init__(self, d_in: int, d_hidden: int, d_out: int):
@@ -24,30 +25,29 @@ class FNN(nn.Module):
         return x
 
 
-def train_model(X, y, d_hidden=10, epochs=20, batch_size=50, lr=1e-3):
+def train_model(X, y, X_val, y_val, d_hidden=10, epochs=20, batch_size=50, lr=1e-3):
     d_in = X.shape[1]
-    d_out = 3 #len(torch.unique(y))
+    d_out = 3
 
     model = FNN(d_in, d_hidden, d_out)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     N = X.shape[0]
-    print("number of rows:",N)
 
     for epoch in range(epochs):
         model.train()
 
-        # shuffle
         perm = torch.randperm(N)
         X_shuffled = X[perm]
         y_shuffled = y[perm]
 
         epoch_loss = 0.0
+        num_batches = 0
 
-        for i in range(0, N, batch_size):
-            xb = X_shuffled[i:i+batch_size]
-            yb = y_shuffled[i:i+batch_size]
+        for j in range(0, N, batch_size):
+            xb = X_shuffled[j:j+batch_size]
+            yb = y_shuffled[j:j+batch_size]
 
             optimizer.zero_grad()
             logits = model(xb)
@@ -56,8 +56,26 @@ def train_model(X, y, d_hidden=10, epochs=20, batch_size=50, lr=1e-3):
             optimizer.step()
 
             epoch_loss += loss.item()
+            num_batches += 1
 
-        #print(f"Epoch {epoch+1}/{epochs}, Loss = {epoch_loss:.4f}")
+        train_loss = epoch_loss / num_batches
+
+        model.eval()
+        with torch.no_grad():
+            logits_val = model(X_val)
+            val_loss = criterion(logits_val, y_val).item()
+
+            preds_val = torch.argmax(logits_val, dim=1)
+            correct_val = (preds_val == y_val).sum().item()
+            total_val = y_val.size(0)
+            val_acc = correct_val / total_val
+
+        print(
+            f"Epoch {epoch+1}/{epochs}, "
+            f"Train Loss = {train_loss:.4f}, "
+            f"Val Loss = {val_loss:.4f}, "
+            f"Val Acc = {val_acc*100:.2f}%"
+        )
 
     return model
 
@@ -74,37 +92,62 @@ def load_model():
     return model
 
 
-def inference(model):
-    df_val = pd.read_excel(r"C:\Users\MonsterPC\Desktop\emgdata\validation_mustafa2903.xlsx")
-    X_val = df_val.iloc[:, :-1].values
-    y_val = df_val.iloc[:, -1].values
-
-    X_val = torch.tensor(X_val, dtype=torch.float32)
-    y_val = torch.tensor(y_val, dtype=torch.long)
+def inference(model, X_val, y_val):
+    criterion = nn.CrossEntropyLoss()
 
     model.eval()
     with torch.no_grad():
-        logits = model(X_val)                 # shape: (84, 3)
-        probs = torch.softmax(logits, dim=1) # shape: (84, 3)
-        preds = probs.argmax(dim=1)          # shape: (84,)
+        logits = model(X_val)
+        probs = torch.softmax(logits, dim=1)
+        preds = probs.argmax(dim=1)
 
+        val_loss = criterion(logits, y_val).item()
         correct = (preds == y_val).sum().item()
         total = y_val.size(0)
         acc = correct / total
 
-    print(f"Correct: {correct}/{total}")
-    print(f"Validation Accuracy: {acc*100:.2f}%")
+        cm = np.zeros((3, 3), dtype=int)
+
+        for t, p in zip(y_val.cpu().numpy(), preds.cpu().numpy()):
+            cm[t, p] += 1
+
+        print("Confusion Matrix:")
+        print(cm)
+
+        per_class_acc = cm.diagonal() / cm.sum(axis=1)
+        print("Per-class accuracy:")
+        for cls, acc_cls in enumerate(per_class_acc):
+            print(f"Class {cls}: {acc_cls*100:.2f}%")
+
+        print(f"Final Validation Loss: {val_loss:.4f}")
+        print(f"Correct: {correct}/{total}")
+        print(f"Validation Accuracy: {acc*100:.2f}%")
+
 
 
 if __name__ == "__main__":
-    df = pd.read_excel(r"C:\Users\MonsterPC\Desktop\emgdata\training_mustafa2903.xlsx")
-    X = df.iloc[:, :-1].values  # ilk 8 sütun
-    y = df.iloc[:, -1].values  # son sütun (TRUECLASS)
+    #df = pd.read_csv(r"/Users/canberkkurtul/Desktop/emgdata/3103/training_normalized_bysubject.csv")
+    df= pd.read_excel(r"/Users/canberkkurtul/Desktop/emgdata/3103/training_data.xlsx")
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+  
+
     X = torch.tensor(X, dtype=torch.float32)
     y = torch.tensor(y, dtype=torch.long)
 
-    model = train_model(X, y, d_hidden=30, epochs=500, batch_size=28, lr=1e-3)
-    inference(model)
+    #df_val = pd.read_csv(r"/Users/canberkkurtul/Desktop/emgdata/3103/validation_normalized.csv")
+    df_val = pd.read_excel(r"/Users/canberkkurtul/Desktop/emgdata/3103/validation_data.xlsx")
 
-    #loaded_model = load_model()
-    #inference(loaded_model)
+    X_val = df_val.iloc[:, :-1].values
+    y_val = df_val.iloc[:, -1].values
+    #print(np.unique(y_val, return_counts=True))
+
+    X_val = torch.tensor(X_val, dtype=torch.float32)
+    y_val = torch.tensor(y_val, dtype=torch.long)
+
+    model = train_model(X, y, X_val, y_val, d_hidden=32, epochs=50, batch_size=500, lr=1e-3)
+    inference(model, X_val, y_val)
+
+    # torch.save(model.state_dict(), "fnn_model.pth")
+    # loaded_model = load_model()
+    # inference(loaded_model, X_val, y_val)
